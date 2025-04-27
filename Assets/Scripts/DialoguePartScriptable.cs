fbfc0bf7e5496d4bbf061143b95c60c6737d616b
Lior Hadashian <liorhadashian@gmail.com>
2025-04-27 08:21:05 +0300

    Post lesson 0 commit

---
+++ Assets/Scripts/DialoguePartScriptable.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DialoguePartScriptable", menuName = "Scriptable Objects/DialoguePartScriptable")]
+public class DialoguePartScriptable : ScriptableObject
+{
+    public string text;
+    
+    public bool hasAudio;
+    public AudioClip audioClip;
+    
+    public string characterName;
+}
