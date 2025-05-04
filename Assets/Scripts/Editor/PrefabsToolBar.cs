ae1a6e118559d9a1987ac18410ca4360aa82966b
Lior Hadashian <liorhadashian@gmail.com>
2025-05-04 17:18:13 +0300

    Delete treasure chest prefab and add custom editor tools.
    The treasure chest prefab has been removed, along with its metadata file. New editor scripts for custom toolbar buttons, an overlay sample, and an item database editor have been added to enhance the Unity editor functionality.

---
+++ Assets/Scripts/Editor/PrefabsToolBar.cs
@@ -0,0 +1,108 @@
+// #if UNITY_2021_2_OR_NEWER
+// using UnityEditor;
+// using UnityEditor.Toolbars;
+// using UnityEditor.Overlays;
+// using UnityEngine;
+// using System.Collections.Generic;
+//
+// [EditorToolbarElement(id: PrefabToolbarOverlayWithCustomIcons.ToolbarElementID)]
+// public class PrefabToolbarOverlayWithCustomIcons : EditorToolbarDropdown
+// {
+//     public const string ToolbarElementID = "PrefabToolbarOverlayWithCustomIcons/PrefabsDropdown";
+//
+//     private static List<GameObject> prefabs = new List<GameObject>();
+//     private static Dictionary<string, Texture2D> prefabIcons = new Dictionary<string, Texture2D>();
+//
+//     public PrefabToolbarOverlayWithCustomIcons()
+//     {
+//         text = "Prefabs";
+//         clicked += ShowDropdown;
+//     }
+//
+//     private void ShowDropdown()
+//     {
+//         GenericMenu menu = new GenericMenu();
+//
+//         if (prefabs.Count == 0)
+//         {
+//             menu.AddDisabledItem(new GUIContent("No prefabs found."));
+//         }
+//         else
+//         {
+//             foreach (var prefab in prefabs)
+//             {
+//                 if (prefab != null)
+//                 {
+//                     GUIContent menuItemContent = new GUIContent(prefab.name, GetCustomPrefabIcon(prefab));
+//
+//                     menu.AddItem(
+//                         menuItemContent,
+//                         false,
+//                         () => InstantiatePrefab(prefab)
+//                     );
+//                 }
+//             }
+//         }
+//
+//         menu.ShowAsContext();
+//     }
+//
+//     private void InstantiatePrefab(GameObject prefab)
+//     {
+//         if (prefab != null)
+//         {
+//             GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+//             Undo.RegisterCreatedObjectUndo(instance, "Instantiate Prefab");
+//             instance.transform.position = Vector3.zero;
+//
+//             Selection.activeObject = instance;
+//         }
+//     }
+//  
+//     private Texture2D GetCustomPrefabIcon(GameObject prefab)
+//     {
+//         if (prefabIcons.TryGetValue(prefab.name, out Texture2D icon))
+//         {
+//             return icon;
+//         }
+//
+//         GUIContent content = EditorGUIUtility.ObjectContent(prefab, prefab.GetType());
+//         return content.image as Texture2D;
+//     }
+//
+//     [InitializeOnLoadMethod]
+//     private static void LoadPrefabsAndIcons()
+//     {
+//         prefabs.Clear();
+//         prefabIcons.Clear();
+//
+//         string[] prefabGuids = AssetDatabase.FindAssets("t:Prefab", new[] { "Assets/Prefabs" });
+//         foreach (string guid in prefabGuids)
+//         {
+//             string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+//             GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+//             if (prefab != null) prefabs.Add(prefab);
+//         }
+//
+//         string[] iconGuids = AssetDatabase.FindAssets("t:Texture2D", new[] { "Assets/Sprites/Items" });
+//         foreach (string guid in iconGuids)
+//         {
+//             string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+//             Texture2D icon = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPath);
+//             if (icon != null)
+//             {
+//                 string iconName = System.IO.Path.GetFileNameWithoutExtension(assetPath);
+//                 prefabIcons[iconName] = icon;
+//             }
+//         }
+//
+//         prefabs.Sort((a, b) => string.Compare(a.name, b.name, System.StringComparison.Ordinal));
+//     }
+// }
+//
+// [Overlay(typeof(SceneView), "Prefab Toolbar Overlay with Custom Icons")]
+// public class PrefabToolbarWithCustomIcons : ToolbarOverlay
+// {
+//     public PrefabToolbarWithCustomIcons() : base(PrefabToolbarOverlayWithCustomIcons.ToolbarElementID) {}
+// }
+// #endif
---
+++ Assets/Scripts/Items/Editor/ItemsDatabaseEditor.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ItemsDatabase))]
+public class ItemsDatabaseEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if (GUILayout.Button("Find all items in assets!"))
+        {
+            UpdateItemsDatabase();
+        }
+    }
+
+    private void UpdateItemsDatabase()
+    {
+        ItemsDatabase itemsDatabase = (ItemsDatabase)target;
+
+        string[] guids = AssetDatabase.FindAssets("t:ItemData");
+        List<ItemData> itemDataList = new List<ItemData>();
+
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            ItemData itemData = AssetDatabase.LoadAssetAtPath<ItemData>(path);
+            if (itemData)
+            {
+                itemDataList.Add(itemData);
+            }
+        }
+        
+        
+        itemsDatabase.items = itemDataList.ToArray();
+        
+        EditorUtility.SetDirty(itemsDatabase);
+        
+        AssetDatabase.SaveAssets();
+    }
+}
